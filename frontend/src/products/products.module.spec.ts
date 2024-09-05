bd11103737c8f1a7ec650743d899da5bb3404d78
srtamol <srta.mol@gmail.com>
2024-09-05 20:25:36 -0300

    Create products.module.spec.ts

---
+++ frontend/src/products/products.module.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
+import { ProductsModule } from './products.module';
+const databaseConnection: SequelizeModuleOptions = {
+dialect: 'sqlite',
+omitNull: true,
+autoLoadModels: true,
+synchronize: true,
+};
+describe('ProductsModule', () => {
+let app: INestApplication;
+beforeEach(async () => {
+const moduleFixture: TestingModule = await Test.createTestingModule({
+imports: [SequelizeModule.forRoot(databaseConnection), ProductsModule],
+}).compile();
+app = moduleFixture.createNestApplication();
+await app.init();
+});
+it('GET /products', async () => {
+return request(await app.getHttpServer())
+.get('/products')
+.expect(200)
+.expect('[]');
+});
+afterAll(async () => {
+await app.close();
+});
+});
