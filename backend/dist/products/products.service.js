37ab99fd44e3cbfda584db4ce87c0cb843e989ea
Roberta <srta.mol@gmail.com>
2024-09-06 01:21:27 +0000

    products commit

--- backend/dist/products/products.service.js
+++ backend/dist/products/products.service.js
@@ -6,19 +6,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.ProductsService = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-const products_module_1 = require("./products/products.module");
-let AppModule = class AppModule {
+let ProductsService = class ProductsService {
+    create(createProductDto) {
+        return 'This action adds a new product';
+    }
+    findAll() {
+        return `This action returns all products`;
+    }
+    findOne(id) {
+        return `This action returns a #${id} product`;
+    }
+    update(id, updateProductDto) {
+        return `This action updates a #${id} product`;
+    }
+    remove(id) {
+        return `This action removes a #${id} product`;
+    }
 };
-exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate([
-    (0, common_1.Module)({
-        imports: [products_module_1.ProductsModule],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
-    })
-], AppModule);
-//# sourceMappingURL=app.module.js.map
+exports.ProductsService = ProductsService;
+exports.ProductsService = ProductsService = __decorate([
+    (0, common_1.Injectable)()
+], ProductsService);
+//# sourceMappingURL=products.service.js.map
