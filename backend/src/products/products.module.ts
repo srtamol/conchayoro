0bbff091e621d20ecc20a02b4a178750007414bd
srtamol <srta.mol@gmail.com>
2024-09-06 19:53:27 -0300

    Update products.module.ts

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -3,9 +3,10 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+
 @Module({
-imports: [SequelizeModule.forFeature([Product])],
-controllers: [ProductsController],
-providers: [ProductsService],
+  imports: [SequelizeModule.forFeature([Product])],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class ProductsModule {}
